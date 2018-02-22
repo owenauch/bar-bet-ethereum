4477f7689706e0b3c6537e16a3415faf65300722
Owen Auch <owenauch@wustl.edu>
2018-02-22 02:18:36 +0000

    remove ganache

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 module.exports = {
   // in order to use Ganache
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
-  }
+  // networks: {
+  //   development: {
+  //     host: "127.0.0.1",
+  //     port: 7545,
+  //     network_id: "*" // Match any network id
+  //   }
+  // }
 };
